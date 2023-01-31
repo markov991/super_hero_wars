3047d034f8fb310cc19015b2510b049f4906dff3
markov991 <markov991@gmail.com>
2023-01-31 14:27:22 +0100

    working on random btn

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const addHeroBtn = document.querySelectorAll(".add-hero-btn");
-const selectedHeroes = document.querySelectorAll(".selected-heroes-container");
+// const selectedHeroes = document.querySelectorAll(".selected-heroes-container");
 
 const searchModal = document.querySelector(".search-modal");
 
@@ -37,7 +37,7 @@ const selectedHeroesInfoModal = function () {
     hero.addEventListener(
       "click",
       () => {
-        console.log(team1[i]);
+        console.log(team1[i], i);
 
         heroInfoModal.classList.remove("hidden");
         renderingHeroStats(team1[i]);
@@ -51,7 +51,7 @@ const selectedHeroesInfoModal = function () {
     hero.addEventListener(
       "click",
       () => {
-        console.log(team2[i]);
+        console.log(team2[i], i);
 
         heroInfoModal.classList.remove("hidden");
         renderingHeroStats(team2[i]);
@@ -114,35 +114,31 @@ const calTeamStrenght = function () {
   }
 };
 
-document
-  .querySelector(".btn-fight")
-  .addEventListener("click", () => calTeamStrenght());
+document.querySelector(".btn-fight").addEventListener("click", () => {
+  calTeamStrenght();
+});
 
 const removingHeroFromTeam = function (team, hero) {
   const removeBtn = document.querySelector(".btn-remove");
 
-  removeBtn.addEventListener("click", () => {
-    if (team.length < 1) {
-      team.splice(1, hero);
+  removeBtn.addEventListener(
+    "click",
+    () => {
+      team.splice(hero, 1);
       heroInfoModal.classList.add("hidden");
       renderingTeams(team1, team1Container);
       renderingTeams(team2, team2Container);
       closingHeroStatsModal();
-    }
-    if (team.length <= 1) {
-      team.pop();
-      heroInfoModal.classList.add("hidden");
-      renderingTeams(team1, team1Container);
-      renderingTeams(team2, team2Container);
-      closingHeroStatsModal();
-    }
-  });
+    },
+    { once: true }
+  );
 };
 
 addHeroBtn.forEach((el, i) => {
   el.addEventListener("click", () => {
     activeSearch = i;
 
+    getRandomHero();
     searchModal.classList.toggle("hidden");
   });
 });
@@ -186,6 +182,7 @@ const searchHero = function (searchPar) {
                 renderingTeams(team1, team1Container);
                 restartingHeroSearchModal();
                 selectedHeroesInfoModal();
+                // toggleingAddHeroBtn()
               }
               if (activeSearch === 1) {
                 addingHeroToTeam(heroSugQuery[index], team2);
@@ -200,6 +197,15 @@ const searchHero = function (searchPar) {
         });
       });
     });
+};
+
+const toggleingAddHeroBtn = function (team, container) {
+  if (team.length >= 5) {
+    container.querySelector(".add-hero-btn").classList.add("hidden");
+  }
+  if (team.length < 5) {
+    container.querySelector(".add-hero-btn").classList.remove("hidden");
+  }
 };
 
 document.addEventListener("keyup", (e) => {
@@ -229,6 +235,37 @@ const restartingHeroSearchModal = function () {
   searchBar.value = "";
 };
 
+const getRandomHero = function () {
+  const randombtn = document.querySelector(".random-icon");
+
+  randombtn.addEventListener("click", () => {
+    console.log("hello");
+    if (activeSearch === 0)
+      fetch("https://akabab.github.io/superhero-api/api/all.json")
+        .then((response) => response.json())
+        .then((allHeros) => {
+          heroInfoModal.classList.remove("hidden");
+
+          renderingHeroStats(
+            allHeros[Math.trunc(Math.random() * allHeros.length)]
+          );
+          closingHeroStatsModal();
+          selectedHeroesInfoModal();
+        });
+    if (activeSearch === 1)
+      fetch("https://akabab.github.io/superhero-api/api/all.json")
+        .then((response) => response.json())
+        .then((allHeros) => {
+          heroInfoModal.classList.remove("hidden");
+          renderingHeroStats(
+            allHeros[Math.trunc(Math.random() * allHeros.length)]
+          );
+          closingHeroStatsModal();
+          selectedHeroesInfoModal();
+        });
+  });
+};
+
 const getFeaturedHeros = function () {
   fetch("https://akabab.github.io/superhero-api/api/all.json")
     .then((response) => response.json())
@@ -243,17 +280,21 @@ const getFeaturedHeros = function () {
 };
 
 const addingHeroToTeam = function (hero, team) {
-  if (!JSON.stringify(team).includes(JSON.stringify(hero))) {
+  if (!JSON.stringify(team).includes(JSON.stringify(hero)) && team.length < 5) {
     team.push(hero);
   }
 };
 
 const renderingTeams = function (team, container) {
-  container.innerHTML = "";
+  const selectedHeroes = container.querySelectorAll(".selected-hero");
+
+  selectedHeroes.forEach((hero) => hero.remove());
+  toggleingAddHeroBtn(team, container);
+  // container.innerHTML = "";
 
   team.forEach((hero) => {
-    container.insertAdjacentHTML(
-      "beforeend",
+    container.querySelector(".add-hero-btn").insertAdjacentHTML(
+      "beforebegin",
       `
       <div class="selected-hero">
       <img
@@ -262,6 +303,7 @@ const renderingTeams = function (team, container) {
         alt=""
         />
         <div class="hero-inf"></div>
+        </div>
         
     `
     );
